b1d8e691b185c5cca1413f74a49cedb9f50a74df
William Gasson <53219297+WilliamGasson@users.noreply.github.com>
2023-04-03 14:51:29 +0100

    Update streamlit_app.py
    editing menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfast menu")
-streamlit.text("Oatmeal")
-streamlit.text("Muffin")
-streamlit.text("Croissant")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
