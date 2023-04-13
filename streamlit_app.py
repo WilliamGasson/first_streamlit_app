101763523825525abf38be61ed48c9e3e224cb8a
William Gasson <53219297+WilliamGasson@users.noreply.github.com>
2023-04-13 16:56:58 +0100

    random s

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get some information")
-  else:s
+  else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     # Take the json version of the response an normalise it
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
