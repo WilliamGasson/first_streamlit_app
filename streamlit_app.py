30c3bc6d92fdd3ab79f6d27c3a9df539297921db
William Gasson <53219297+WilliamGasson@users.noreply.github.com>
2023-04-13 15:21:51 +0100

    Update streamlit_app.py
    tidy up display

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,24 +10,28 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# add a pick list
+# Add a pick list
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),["Avocado", "Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#display table on page
+# Display table on page
 streamlit.dataframe(fruits_to_show)
+
+
 
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response)
 
 # Take the json version of the response an normalise it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
