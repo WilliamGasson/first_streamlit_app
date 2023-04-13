cccd16c17d7a8f3b5120565d4d56c0e4e09b071e
William Gasson <53219297+WilliamGasson@users.noreply.github.com>
2023-04-13 16:38:10 +0100

    loading the rest of the data

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,12 +53,12 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 # streamlit.text("Hello from Snowflake:")
 streamlit.header("The fuit load list contains:")
 
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
 
 
 
